eea5e55f156769a623fd6334a3e2fd4215c63936
plumliil <476453574@qq.com>
2022-11-10 23:38:58 +0800

    dropdown.vue

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="dropdown">
+    <a href="#" class="btn btn-outline-light my-2 dropdowm-toggle">{{
+      title
+    }}</a>
+    <ul class="dropdown-menu">
+      <li><a class="dropdown-item" href="#">Action</a></li>
+      <li><a class="dropdown-item" href="#">Another action</a></li>
+      <li><a class="dropdown-item" href="#">Something else here</a></li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
+
+<style lang="less" scoped></style>
