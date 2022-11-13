7d926f3143ae540b3f1c5ced3599ef90b19110f1
plumliil <476453574@qq.com>
2022-11-13 23:38:35 +0800

    Entry file Ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "bootstrap/dist/css/bootstrap.min.css";
+import router from "./route";
+import store from "./store";
+
+console.log("store", store.state);
 
 const app = createApp(App);
-
+app.use(router);
+app.use(store);
 app.mount("#app");
