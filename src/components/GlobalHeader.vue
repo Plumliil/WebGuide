423bc6a7f2334ee83e3fe1470b1ddcbd58569bca
plumliil <476453574@qq.com>
2022-11-13 23:37:11 +0800

    GlobalHeader component

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,8 +1,18 @@
 <template>
   <!-- <nav class="navbar bg-light"> -->
-  <nav class="navbar navbar-dark bg-primary justy-content-between mb-4">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">
+  <nav
+    class="navbar navbar-dark position-relative bg-primary justy-content-between mb-2"
+    style="height: 45px"
+  >
+    <div class="container-fluid mt-0">
+      <router-link
+        to="/"
+        class="navbar-brand position-absolute top-0 start-10"
+        href="#"
+      >
+        <li class="list-inline-item">
+          <a href="#" class="btn" style="pointer-events: none"></a>
+        </li>
         <img
           src="https://s2.loli.net/2022/11/10/FdNhjf968RzsWIx.jpg"
           alt="Logo"
@@ -11,28 +21,66 @@
           class="d-inline-block align-text-top"
         />
         Bootstrap
-      </a>
-      <ul v-if="!user.isLogin" class="list-inline mb-0">
+      </router-link>
+      <div
+        class="form-group position-absolute top-50 start-50 translate-middle"
+      >
+        <input
+          type="text"
+          class="form-control input-mini"
+          @input="updateValue"
+          placeholder="Search"
+          style="height: 35px; outline: none"
+        />
+      </div>
+      <ul
+        v-if="!user.isLogin"
+        class="list-inline position-absolute top-0 end-0"
+      >
         <li class="list-inline-item">
-          <a href="#" class="btn btn-outline-light my-2">登录</a>
+          <router-link to="/login" class="btn btn-sm btn-outline-light my-1"
+            >登录</router-link
+          >
         </li>
         <li class="list-inline-item">
-          <a href="#" class="btn btn-outline-light my-2">注册</a>
+          <router-link to="/signin" class="btn btn-sm btn-outline-light my-1"
+            >注册</router-link
+          >
+        </li>
+        <li class="list-inline-item">
+          <a href="#" class="btn" style="pointer-events: none"></a>
         </li>
       </ul>
-      <ul v-if="user.isLogin" class="list-inline mb-0">
+      <ul
+        v-if="user.isLogin"
+        class="list-inline mb-0 position-absolute top-0 end-0"
+      >
         <li class="list-inline-item">
           <dropdown :title="`你好 ${user.name}`">
             <dropdown-item
-              ><a href="#" class="dropdown-item">新建</a></dropdown-item
-            >
-            <dropdown-item disabled
-              ><a href="#" class="dropdown-item">编辑</a></dropdown-item
+              ><router-link to="/createPost" class="dropdown-item"
+                >贡献</router-link
+              ></dropdown-item
             >
             <dropdown-item
-              ><a href="#" class="dropdown-item">查询</a></dropdown-item
+              ><router-link to="/userContribute" class="dropdown-item"
+                >我的贡献</router-link
+              ></dropdown-item
+            >
+            <dropdown-item
+              ><router-link to="/userCollect" class="dropdown-item"
+                >收藏</router-link
+              ></dropdown-item
+            >
+            <dropdown-item
+              ><router-link to="/login" class="dropdown-item" @click="logout"
+                >退出</router-link
+              ></dropdown-item
             >
           </dropdown>
+        </li>
+        <li class="list-inline-item">
+          <a href="" class="btn"></a>
         </li>
       </ul>
     </div>
@@ -40,27 +88,56 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from "vue";
+import { defineComponent, PropType, reactive } from "vue";
 import Dropdown from "./Dropdown.vue";
 import DropdownItem from "./DropdownItem.vue";
-export interface UserProps {
-  isLogin: boolean;
-  name?: string;
-  id?: number;
-}
+import { GlobalDataProps, UserProps } from "../interface";
+import { useStore } from "vuex";
+
 export default defineComponent({
   name: "GlobalHeader",
   components: {
     Dropdown,
     DropdownItem,
   },
+  emits: ["update:modelValue"],
   props: {
     user: {
       type: Object as PropType<UserProps>,
       required: true,
     },
+    modelValue: String,
+  },
+  setup(props, ctx) {
+    const store = useStore<GlobalDataProps>();
+    const inputRef = reactive({
+      val: props.modelValue || "",
+      error: false,
+      message: "",
+    });
+    const updateValue = (e: Event) => {
+      const targetValue = (e.target as HTMLInputElement).value;
+      inputRef.val = targetValue;
+      ctx.emit("update:modelValue", targetValue);
+    };
+    const logout = () => {
+      store.commit("logout");
+    };
+    return {
+      updateValue,
+      logout,
+    };
   },
 });
 </script>
 
-<style lang="less" scoped></style>
+<style>
+.navbar-nav > li > a {
+  padding-top: 4px;
+  padding-bottom: 0;
+  height: 28px;
+}
+.navbar {
+  min-height: 20px;
+}
+</style>
