1860046b6f9ce82550ed7fbd7e8aa76ec553d498
plumliil <476453574@qq.com>
2022-11-15 23:33:51 +0800

    globalheader component

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -13,25 +13,26 @@
         <li class="list-inline-item">
           <a href="#" class="btn" style="pointer-events: none"></a>
         </li>
-        <img
+        <!-- <img
           src="https://s2.loli.net/2022/11/10/FdNhjf968RzsWIx.jpg"
           alt="Logo"
           width="30"
           height="24"
           class="d-inline-block align-text-top"
-        />
-        Bootstrap
+        /> -->
+        <svg t="1668500696509" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="10554" width="32" height="32"><path d="M810.666667 247.04l-217.6 544-89.6-179.2A128 128 0 0 0 444.16 554.666667L264.533333 464.64 810.666667 247.04M885.333333 128a42.666667 42.666667 0 0 0-15.786666 3.413333l-725.333334 290.133334a42.666667 42.666667 0 0 0-2.986666 77.226666l264.533333 132.266667a42.666667 42.666667 0 0 1 19.2 19.2l132.266667 264.533333A42.666667 42.666667 0 0 0 597.333333 938.666667a42.666667 42.666667 0 0 0 39.68-26.453334l289.706667-725.333333A42.666667 42.666667 0 0 0 885.333333 128z" fill="#e6e6e6" p-id="10555"></path></svg>
+        WebGuide
       </router-link>
       <div
         class="form-group position-absolute top-50 start-50 translate-middle"
       >
-        <input
+        <!-- <input
           type="text"
           class="form-control input-mini"
           @input="updateValue"
           placeholder="Search"
           style="height: 35px; outline: none"
-        />
+        /> -->
       </div>
       <ul
         v-if="!user.isLogin"
